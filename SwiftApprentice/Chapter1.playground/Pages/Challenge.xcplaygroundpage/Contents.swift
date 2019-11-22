85ac6ab466977bfd35717f7cb5b38974cb4dde97
Victoria Li <VLi@lcs.on.ca>
2019-11-22 15:30:56 -0500

    chapter 1 challenge int and var math operation

--- SwiftApprentice/Chapter1.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ SwiftApprentice/Chapter1.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -2,5 +2,50 @@ import Cocoa
 
 var str = "Hello, playground"
 
+//chalenge 1
+let myAge = 15
+var dogs = 1
+dogs += 1
+
+//chalenge 2
+var age: Int = 16
+print(age)
+age = 30
+print(age)
+
+//chalenge 3
+let x: Int = 46
+let y: Int = 10
+let answer1: Int = (x * 100) + y
+let answer2: Int = (x * 100) + (y * 100)
+let answer3: Int = (x * 100) + (y / 10)
+
+//challenge 4
+print((8 - 4) * (2 + 6) / (3 * 4))
+
+//challenge 5
+let rating1 = 10
+let rating2 = 20
+let rating3 = 50
+let averageRating = (rating1 + rating2 + rating3)/3
+
+//challenge 6
+let voltage: Double = 10
+let current: Double = 20
+let power: Double = voltage * current
+
+//challenge 7
+let resistance: Double = power/(current * current)
+
+//challenge 8
+let randomNumber = arc4random()
+let diceRoll = Int(randomNumber/1000000000)
+
+//challenge 9
+let a: Double = 1
+let b: Double = 2
+let c: Double = 3
+let root1 = (- b + √(b² - 4 * a * c)) / (2 * a)
+let root2 = (- b - √(b² - 4 * a * c)) / (2 * a)
 
 
