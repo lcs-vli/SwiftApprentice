d1fa243bfa44557d654770ce5987a6eb67924e13
Victoria Li <VLi@lcs.on.ca>
2019-11-22 15:07:02 -0500

    chapter 1 mini exercise

---
+++ SwiftApprentice/Chapter1.playground/Pages/Miniexercise.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+
+//: [Next](@next)
+let myAge = 15
+var averageAge = 22.5
+let testNumber = 3
+let evenOdd = 1
+var answer = 0
